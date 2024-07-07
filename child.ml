c43beea5d160be595b92f5f02fdb50980d841cb7
Oleksander Sankin <oleksander.sankin@sandbx.co>
2024-07-07 12:47:20 +0300

    Add sending 'keep alive' message to server

--- child.ml
+++ child.ml
@@ -2,6 +2,8 @@ open Unix
 
 let server_socket = ref None
 
+let random_number l r = Random.self_init (); l + Random.int r
+
 let connect_to_server server_ip server_port =
   let sock = socket PF_INET SOCK_STREAM 0 in
   let server_addr = inet_addr_of_string server_ip in
@@ -10,34 +12,45 @@ let connect_to_server server_ip server_port =
   connect sock server_sockaddr;
   server_socket := Some sock
 
-(*   let message_bytes = Bytes.of_string message in
-  let bytes_sent = send sock message_bytes 0 (Bytes.length message_bytes) [] in
-  Printf.printf "Sent %d bytes to the server\n" bytes_sent; *)
-
 let try_connect_to_server server_ip server_port =
   while !server_socket = None do
     try
       connect_to_server server_ip server_port;
 	with e ->
-	  Printf.printf "Reconnect after 5 secs\n%!";
-	  sleep 5
+	  Printf.printf "Reconnect after 3 secs\n%!";
+	  sleep 3
   done
 
 let close_server_socket () =
-  match !server_socket with
-  | Some sock ->
-      close sock;
-      Printf.printf "Server socket closed\n";
-      server_socket := None
-  | None ->
-      Printf.printf "No server socket to close\n"
+  try
+    match !server_socket with
+	  | Some sock ->
+	      close sock;
+	      Printf.printf "Server socket closed\n";
+	      server_socket := None
+	  | None ->
+	      Printf.printf "No server socket to close\n"
+  with e -> ()
 
-let random_number l r = Random.self_init (); l + Random.int r
+let send_to_server message =
+	try
+	  match !server_socket with
+	  | Some sock ->
+		  let message_bytes = Bytes.of_string message in
+		  let bytes_sent = send sock message_bytes 0 (Bytes.length message_bytes) [] in
+		  Printf.printf "Sent %d bytes to the server\n" bytes_sent
+	  | None ->
+	      Printf.printf "No server socket to send\n"
+    with e ->
+	  close_server_socket ();
+	  exit 0
 
 let keep_alive () = 
   while true do
     let x = random_number 5 20 in sleep x;
-    Printf.printf "Sleep \n%!";
+    let pid = getpid () in 
+    let alive = "PID: " ^ string_of_int pid ^ " ALIVE." in
+    send_to_server alive
   done
 
 let shutdown () = 
